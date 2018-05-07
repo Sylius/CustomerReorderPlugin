1adcc0577f33118df9efef11369ca94b9e41348e
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-05-07 13:35:36 +0200

    phpstan.neon + configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\CustomerReorderPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('sylius_customer_reorder_plugin');
+
+        return $treeBuilder;
+    }
+}

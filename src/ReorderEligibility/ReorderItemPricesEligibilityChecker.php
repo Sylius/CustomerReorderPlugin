dc622bb9a01e8f013ddac38dc10c8507ead7efc4
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-05-30 12:22:25 +0200

    Improved behat tests and flash messages

--- src/ReorderEligibility/ReorderItemPricesEligibilityChecker.php
+++ src/ReorderEligibility/ReorderItemPricesEligibilityChecker.php
@@ -9,6 +9,15 @@ use Sylius\Component\Core\Model\OrderItemInterface;
 
 final class ReorderItemPricesEligibilityChecker implements ReorderEligibilityChecker
 {
+    /** @var ReorderEligibilityConstraintMessageFormatterInterface */
+    private $reorderEligibilityConstraintMessageFormatter;
+
+    public function __construct(
+        ReorderEligibilityConstraintMessageFormatterInterface $reorderEligibilityConstraintMessageFormatter
+    ) {
+        $this->reorderEligibilityConstraintMessageFormatter = $reorderEligibilityConstraintMessageFormatter;
+    }
+
     public function check(OrderInterface $order, OrderInterface $reorder)
     {
         $orderVariantNamesToTotal = [];
@@ -24,19 +33,28 @@ final class ReorderItemPricesEligibilityChecker implements ReorderEligibilityChe
             $reorderVariantNamesToTotal[$reorderItem->getVariantName()] = $reorderItem->getUnitPrice();
         }
 
-        foreach (array_keys($orderVariantNamesToTotal) as $key) {
-            if (!array_key_exists($key, $reorderVariantNamesToTotal)) {
+        $orderVariantsWithChangedPrice = [];
+
+        foreach (array_keys($orderVariantNamesToTotal) as $variantName) {
+            if (!array_key_exists($variantName, $reorderVariantNamesToTotal)) {
                 continue;
             }
 
-            if ($orderVariantNamesToTotal[$key] !== $reorderVariantNamesToTotal[$key]) {
-                return [
-                    'type' => 'info',
-                    'message' => 'sylius.reorder.items_price_changed'
-                ];
+            if ($orderVariantNamesToTotal[$variantName] !== $reorderVariantNamesToTotal[$variantName]) {
+                array_push($orderVariantsWithChangedPrice, $variantName);
             }
         }
 
-        return [];
+        if (empty($orderVariantsWithChangedPrice)) {
+            return [];
+        }
+
+        return [
+            'type' => 'info',
+            'message' => 'sylius.reorder.items_price_changed',
+            'parameters' => [
+                '%order_items%' => $this->reorderEligibilityConstraintMessageFormatter->format($orderVariantsWithChangedPrice),
+            ]
+        ];
     }
 }
